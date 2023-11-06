0bc3a9e63571443a6e59d90037ea40488f8c4c96
Sizan458 <www.shijan23@gmail.com>
2023-11-06 21:06:33 +0600

    create a collection in data base

--- index.js
+++ index.js
@@ -25,13 +25,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+     //create a database
+     const All_ASSIGNMENT =client.db('Study-ally ').collection("all-assignments");
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
